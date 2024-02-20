142e92cf0e7c7c2f5ac72bad20a13888b3a756cc
Magret <oyedeleabiodun79@gmail.com>
2024-02-20 08:22:10 -0800

    Solution to task 10

--- models/base_model.py
+++ models/base_model.py
@@ -16,21 +16,23 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
+        if not kwargs or 'id' not in kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             # storage.new(self)
+            if kwargs:
+                for k, v in kwargs.items():
+                    setattr(self, k, v)
+            self.save()
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
-            # self.__dict__.update(kwargs)
-            for key, value in kwargs.items():
-                setattr(self, key, value)
+            self.__dict__.update(kwargs)
         """
         if kwargs:
             from models import storage
--- models/user.py
+++ models/user.py
@@ -13,8 +13,8 @@ class User(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+        first_name = Column(String(128))
+        last_name = Column(String(128))
 
         # Define the relationship with the Place class and Review class
         places = relationship('Place', backref='user',
